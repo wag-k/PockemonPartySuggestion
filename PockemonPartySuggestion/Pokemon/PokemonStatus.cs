79869d60f430e178bc492f05852621812a683121
wag_k <captenkenta@gmail.com>
2021-12-19 15:53:39 +0900

    feat: ポケモンのステータス追加

--- PockemonPartySuggestion/Pokemon/PokemonStatus.cs
+++ PockemonPartySuggestion/Pokemon/PokemonStatus.cs
@@ -10,6 +10,18 @@ namespace PokemonPartySuggestion.Pokemon
     {
         public String No {get; set;}
         public String Name {get; set;}
-        public PokemonType Type1 {get; set;} 
+        public PokemonType[] Types {get; set;} 
+        public String[] Abilities {get; set;}
+        public BaseStatus Status {get; set;}
+    }
+
+    public struct BaseStatus{
+        public int HP{get; set;}
+        public int Atk{get; set;}
+        public int Def{get; set;}
+        public int SpAtk{get; set;}
+        public int SpDef{get; set;}
+        public int Speed{get; set;}
+        public int Sum{get; set;}
     }
 }
